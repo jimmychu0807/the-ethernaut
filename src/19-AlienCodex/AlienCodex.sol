a9750545a4e38a7859895d5071e24098b9874247
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2026-02-04 12:02:12 +0800

    updated

---
+++ src/19-AlienCodex/AlienCodex.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract AlienCodex {
+    bool public contact;
+    bytes32[] public codex;
+
+    modifier contacted() {
+        assert(contact);
+        _;
+    }
+
+    function makeContact() public {
+        contact = true;
+    }
+
+    function record(bytes32 _content) public contacted {
+        codex.push(_content);
+    }
+
+    function retract() public contacted {
+        codex.length--;
+    }
+
+    function revise(uint256 i, bytes32 _content) public contacted {
+        codex[i] = _content;
+    }
+}
